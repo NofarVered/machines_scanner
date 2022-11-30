0a9a9ff452eac022ffb6500664d6239b10082b28
NofarVered <nofarss79@gmail.com>
2022-11-30 22:56:19 +0200

    config folder

--- src/backend/models/account.py
+++ src/backend/models/account.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 class Account(BaseModel):
     account_id: int
     account_name: str
-    operating_platform: str
     privilege: bool
     set_password_date: str
     scan_transaction: int
