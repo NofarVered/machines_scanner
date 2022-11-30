4ebb9869cce729728ef39dfb08de65194c6676f9
NofarVered <nofarss79@gmail.com>
2022-11-30 22:28:14 +0200

    config folder

---
+++ config/constants.py
@@ -0,0 +1,8 @@
+drop_db = ""
+create_db = ""
+use_db = ""
+
+mock_accounts = "./mocks/accounts.json"
+mock_scanners = "./mocks/scanners.json"
+mock_scan_transactions = "./mocks/scan_transactions.json"
+mock_it_managers = "./mocks/it_managers.json"
---
+++ config/create_db_script.py
@@ -0,0 +1,62 @@
+import pymysql
+import json
+from constants import *
+
+CONNECTOR = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="machines_scanner",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def create_database():
+    try:
+        with CONNECTOR.cursor() as cursor:
+            cursor.execute(drop_db)
+            cursor.execute(create_db)
+            CONNECTOR.commit()
+    except Exception as e:
+        print(e)
+
+
+def create_all_tables():
+    try:
+        with CONNECTOR.cursor() as cursor:
+            cursor.execute(use_db)
+            # code
+            CONNECTOR.commit()
+    except Exception as e:
+        print(e)
+
+
+def load_data():
+    data = json_processor()
+    # code
+    try:
+        with CONNECTOR.cursor() as cursor:
+            # code
+            CONNECTOR.commit()
+            CONNECTOR.close()
+    except Exception as e:
+        print(e)
+
+
+def json_processor(file_name):
+    data_file = open(file_name)
+    data = json.load(data_file)
+    data_file.close()
+    return data
+
+
+# python create_db_script.py
+if __name__ == "__main__":
+    print("--- START creating DB Bank_manager")
+    create_database()
+    print("--- START creating tables")
+    create_all_tables()
+    print("--- DATABSE IS READY")
+    load_data()
+    print("--- DONE LOAD DATA")
