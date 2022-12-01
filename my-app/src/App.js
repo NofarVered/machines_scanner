8fdc9e2c01457c51fc3e7770a2fbf13271ec5016
amit ben basat <amitbasat124@gmail.com>
2022-12-01 14:17:31 +0200

    adding part of the pie charm

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { ApexChartPrivilege } from "./features/piCharts/piCahrtsPrivilege";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ApexChartPrivilege/>
+    </div>
+  );
+}
+
+export default App;
