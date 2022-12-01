b1ee27b4584e2f1c9f06e31f63c564effa231feb
amit ben basat <amitbasat124@gmail.com>
2022-12-01 11:40:55 +0200

    adding the cpm

--- cpm.py
+++ cpm.py
@@ -1,6 +1,7 @@
 import requests
 
 URL = "http://localhost:8000/scans/"
+URL_CHANGE_STATUS = "http://localhost:8000/scans/status"
 
 ScanerManager=0
 
@@ -9,9 +10,11 @@ def cpm_ask_for_active_scan():
     while flag:
         scnas = requests.get(url = URL)
         all_scans = scnas.json()
-        active_sacn=[scan for scan in all_scans if scan.active==True]
+        active_sacn=[scan for scan in all_scans if scan.status=="Active"]
         if(len(active_sacn)!=0):
-            ScanerManager.getallusers(); 
+            scnas = requests.post(url = URL,json={"status":"pending"})    
+            for scan in active_sacn:
+                ScanerManager.getallusers(scan); 
 
 
 
--- getting_all_the_users_windows.py
+++ getting_all_the_users_windows.py
@@ -2,8 +2,36 @@
 import wmi
 c = wmi.WMI("","","")
 
+priviliged_users=[]
+nonpriviliged=[]
+
+def get_users_of_machine(): 
+    for user in c.Win32_UserAccount(): 
+        if check_if_priviliged(user):
+            priviliged_users.append(user)
+        else:
+            nonpriviliged.append(user)
+
+
+def check_if_priviliged(user):
+    if user["Name"]=="Administrator":
+        return True;
+    return False
+    
+
+
+def see_users_groups():
+    for group in c.Win32_Group(): 
+        print(group.Caption) 
+    for user in group.associators(wmi_result_class="Win32_UserAccount"): 
+        print(" [+]", user.Caption)
+
+
 for u in c.Win32_UserAccount(["Name"]): #Net
     print (u.Name)
+
+
+
 
 
 
