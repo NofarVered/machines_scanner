f33725851ab305fd9b743dc52a4cadc6dd8dd8c9
eladshachar <elad.shachar@gmail.com>
2022-12-01 18:25:49 +0200

    routing to accounts

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './features/Dashboard';
-
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <Dashboard/>
-    </div>  
+    <Router>
+      <div className="App">
+         <Dashboard/>
+      </div>
+    </Router>   
   );
 }
 
