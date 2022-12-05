bdbdcb93054c520acb180c62901eb65759b8611b
eladshachar <elad.shachar@gmail.com>
2022-12-05 11:07:36 +0200

    mock CPM

--- client/src/features/CpmWrapper.js
+++ client/src/features/CpmWrapper.js
@@ -12,7 +12,7 @@ export default function CpmWrapper() {
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                        <CpmSelection />
+                        <CpmSelection /> 
                     </Paper>
                 </Grid>     
             </Grid>
--- client/src/features/cpmSelection/mockCPM.js
+++ client/src/features/cpmSelection/mockCPM.js
@@ -1,19 +1,22 @@
-const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
+const mockCPM = [
+    {
+        name: "CPM 1",
+        pic: 3
     },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
+    {
+        name: "CPM 2",
+        pic: 3
     },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
+    {
+        name: "CPM 3",
+        pic: 3
     },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+    {
+        name: "CPM 4",
+        pic: 3
+    },
+    {
+        name: "CPM 5",
+        pic: 3
+    },
+]
