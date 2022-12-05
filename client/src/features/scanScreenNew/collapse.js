17a4a81c5a74c55da0c354c9d2f15b2a34a21076
amit ben basat <amitbasat124@gmail.com>
2022-12-05 12:12:37 +0200

    adding the collapse itself to the row

--- client/src/features/scanScreenNew/collapse.js
+++ client/src/features/scanScreenNew/collapse.js
@@ -43,80 +43,31 @@ function Row(props) {
 
   return (
     <React.Fragment>
-      <TableRow
-                                    hover
-                                    role="checkbox"
-                                    
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 }}}
-                                    aria-checked={isItemSelected}
-                                    tabIndex={-1}
-                                    key={row.trackingNo}
-                                    selected={isItemSelected}
-                                >
-                                    <TableCell component="th" id={labelId} scope="row" align="center">
-                                            
-                                            {row.ScanName}
-                                       
-                                    </TableCell>
-                                    <TableCell align="center">{row.excuteBy}</TableCell>
-                                    <TableCell align="center">{row.SuucesDate}</TableCell>
-                                    <TableCell align="center">
-                                        <OrderStatus status={row.Status} />
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <NumberFormat value={row.protein} displayType="text" thousandSeparator prefix="$" />
-                                    </TableCell>
-                                    <TableCell align="center">
-                                    <IconButton
-                                        aria-label="expand row"
-                                        size="small"
-                                        onClick={() => setOpen(!open)}
-                                    >
-                                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                    </IconButton>
-                                    </TableCell>
-
-
-                                    
-                                </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
         </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.fat}</TableCell>
+        <TableCell align="right">{row.carbs}</TableCell>
+        <TableCell align="right">{row.protein}</TableCell>
       </TableRow>
+      
+        
+      
     </React.Fragment>
   );
 }
+
 
 Row.propTypes = {
   row: PropTypes.shape({
