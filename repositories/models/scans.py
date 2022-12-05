428cb23d35e9baebe24fa22bea5c63ea5fe2206e
Or Geisler <orgeisler1@gmail.com>
2022-12-05 10:43:12 +0200

    scans route

--- repositories/models/scans.py
+++ repositories/models/scans.py
@@ -2,11 +2,10 @@ from pydantic import BaseModel
 
 
 class Scans(BaseModel):
-    transaction_id: int
-    transaction_name: str
+    scan_id: int
+    scan_name: str
+    success_date: str
+    excute_by: str
     status: str
-    creation_time: str
-    last_run_time: str
-    csv_file: str
-    scanner_name: str
-    it_manager_id: int
+    scan_file: str
+
