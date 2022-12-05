00d8ed27bee95b7642b39dd4551dab94a93ef12f
Or Geisler <orgeisler1@gmail.com>
2022-12-05 10:29:57 +0200

    querys

--- repositories/querys.py
+++ repositories/querys.py
@@ -1,12 +1,19 @@
-sql_select_all_users =    """
+sql_select_all_cpms =    """
                            SELECT *
-                           FROM accounts
+                           FROM cpms
                            """
 
-sql_select_all_users_by_machine =    """
-                                    SELECT *
-                                    FROM accounts
-                                    WHERE accounts.machine_id = %s
+sql_select_all_recent_accounts =    """
+                                    SELECT a.account_name , a.scan_id , a.is_privileged , a.group_name , a.password_age
+                                    FROM accounts a JOIN scan_requests sr ON a.scan_id = sr.scan_id
+                                    WHERE sr.is_most_recent = true
+                                    """
+
+
+sql_select_all_account_by_machine =    """
+                                    SELECT a.account_name , a.scan_id , a.is_privileged , a.group_name , a.password_age
+                                    FROM accounts a JOIN machine_accounts ma ON a.account_name = ma.account_name
+                                    WHERE ma.machine_id = %s
                                     """
 
 sql_select_all_privilliged_machine =    """
