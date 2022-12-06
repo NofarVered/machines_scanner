910155421266ecbd736f47f4e121bed90adf131f
MatanNahum <matan.nahum95@gmail.com>
2022-12-06 12:42:13 +0200

    get accounts added

--- manager/scans_manager.py
+++ manager/scans_manager.py
@@ -1,39 +1,43 @@
-from fastapi import APIRouter , status , Response ,Request
+from fastapi import APIRouter, status, Response, Request
 from fastapi.responses import JSONResponse
 from repositories import querys
-from repositories.sql_wrapper import db_wrapper 
+from repositories.sql_wrapper import db_wrapper
 from repositories.models.scans import Scans
-from repositories.scans_repository import Scans_repo
 
 db = db_wrapper()
 scans = APIRouter()
 
 
+@scans.post("/scans", response_class=JSONResponse, status_code=status.HTTP_201_CREATED)
+def addScan(scan: Scans):
+    db.execute_insert_query(
+        querys.sql_insert_scan,
+        (
+            scan.transaction_id,
+            scan.transaction_name,
+            scan.status,
+            scan.csv_file,
+            scan.scanner_name,
+            scan.last_run_time,
+            scan.user_login,
+        ),
+    )
+    return {"success": True, "payload": scan}
 
-@scans.post('/scans', response_class= JSONResponse , status_code= status.HTTP_201_CREATED)
-def addScan(scan:Scans):
-    Scans_repo.addScan()
-    return {
-            "success": True,
-            "payload": scan
-            }
 
-@scans.get('/scans',response_class= JSONResponse , status_code= status.HTTP_200_OK)
+@scans.get("/scans", response_class=JSONResponse, status_code=status.HTTP_200_OK)
 def getScans():
+    # scans_list = db.execute_select_all_query(querys.sql_select_all_scans)
+    scans_list = [Scans(1, "test", "1.1.1990", "or", "pending", "file.csv")]
+    return {"success": True, "payload": scans_list}
 
-    # scans_list = [Scans(1,"1.1.1990","Or","test","pending","csv.file",True,1) ]
-    scans = Scans_repo.getScan()
-    return {
-            "success": True,
-            "payload": scans
-            }
 
-@scans.post('/scans/{scan_transaction_id}', response_class= JSONResponse , status_code= status.HTTP_201_CREATED)
-async def addScan(request: Request): 
+@scans.post(
+    "/scans/{scan_transaction_id}",
+    response_class=JSONResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+async def addScan(request: Request):
     req = await request.json()
     ## need to implement the change status logic
-    return {
-
-            "success": True,
-            "payload": req
-            }
+    return {"success": True, "payload": req}
--- repositories/accounts_repository.py
+++ repositories/accounts_repository.py
@@ -1,4 +1,35 @@
+from repositories.models.account import Account
+from repositories.sql_wrapper import db_wrapper
+
 sql_select_all_accounts = """
                            SELECT *
                            FROM accounts
                         """
+
+db = db_wrapper()
+
+
+class Accounts_repo:
+    #  def getAllAccountsByMachines(self, account: Scans):
+    #      db.execute_insert_query(
+    #          insert_to_scan_requests_table,
+    #          (
+    #              scan.cpm_id,
+    #              scan.success_date,
+    #              scan.execute_by,
+    #              scan.scan_name,
+    #              scan.cpm_id,
+    #              scan.scan_status,
+    #              scan.scan_file,
+    #              scan.is_most_recent,
+    #          ),
+    #      )
+    #      return scan
+
+    def getAllAccounts():
+        accounts = db.execute_select_all_query(sql_select_all_accounts)
+        return accounts
+
+
+# account_repo = Accounts_repo()
+# account_repo.getAllAccounts()
