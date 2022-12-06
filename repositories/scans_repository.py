7350a89069b123586f541187c905a19bba818060
eladshachar <elad.shachar@gmail.com>
2022-12-06 14:40:43 +0200

    fixed a server bug

--- repositories/scans_repository.py
+++ repositories/scans_repository.py
@@ -3,8 +3,8 @@ from repositories.sql_wrapper import db_wrapper
 
 
 insert_to_scan_requests_table = """
-                                INSERT IGNORE into scan_requests (scan_id, success_date, execute_by, scan_name, cpm_id, scan_status, scan_file, is_most_recent)
-                                values (%s , %s, %s, %s, %s, %s, %s, %s)
+                                INSERT IGNORE into scan_requests (success_date, execute_by, scan_name,scan_status,scan_file,is_most_recent, cpm_id)
+                                values (%s, %s, %s, %s, %s, %s, %s)
                                 """
 
 
@@ -16,12 +16,10 @@ sql_select_all_scans = """
 db = db_wrapper()
 
 class Scans_repo:
-
-    def addScan(self,scan:Scans):
-        db.execute_insert_query(insert_to_scan_requests_table,(scan.cpm_id,scan.success_date,scan.execute_by,scan.scan_name,scan.cpm_id,scan.scan_status,scan.scan_file,scan.is_most_recent))
+    def addScan(scan:Scans):
+        db.execute_insert_query(insert_to_scan_requests_table,(scan["success_date"],scan['execute_by'],scan['scan_name'],scan['scan_status'],scan['scan_file'],scan['is_most_recent'],scan['cpm_id']))
         return scan
     
     def getScan():
         scans = db.execute_select_all_query(sql_select_all_scans)
         return scans
-
