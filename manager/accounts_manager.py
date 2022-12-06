ff99208ed8a04915efa2b8e2e159209b1f7be224
NofarVered <nofarss79@gmail.com>
2022-12-06 15:03:43 +0200

    fixed mock accounts file

--- manager/accounts_manager.py
+++ manager/accounts_manager.py
@@ -1,28 +1,35 @@
-from fastapi import APIRouter , status , Response ,Request
+from fastapi import APIRouter, status, Response, Request
 from fastapi.responses import JSONResponse
 from repositories import querys
-from repositories.sql_wrapper import db_wrapper 
+from repositories.sql_wrapper import db_wrapper
 from repositories.models.account import Account
+from repositories.accounts_repository import get_accounts
 
 db = db_wrapper()
 account = APIRouter()
 
-@account.get('/accounts',response_class= JSONResponse , status_code= status.HTTP_200_OK)
+# @account.get('/accounts',response_class= JSONResponse , status_code= status.HTTP_200_OK)
+# def getAccounts():
+#     users_list = db.execute_select_all_query(querys.sql_select_all_users)
+#     users_list = [Account("Or",1,True,"group_1",100)]
+#     return users_list
+
+# @account.get('/accounts/{machine_id}',response_class= JSONResponse , status_code= status.HTTP_200_OK)
+# def getAccountsByMachine(machine_id):
+#     users_list = db.execute_select_all_query(querys.sql_select_all_users_by_machine)
+#     # users_list = [User("Or",True,"01/12/2022",1,machine_id,"best group")]
+#     return users_list
+
+# @account.get('/accounts/stats',response_class= JSONResponse , status_code= status.HTTP_200_OK)
+# def getAccountsStats():
+#     privilliged_stats = db.execute_select_all_query(querys.sql_select_all_privilliged_machine)
+#     stats = {
+#                 "privilliged_stats": privilliged_stats
+#             }
+#     return stats
+
+
+@account.get('/accounts', response_class=JSONResponse, status_code=status.HTTP_200_OK)
 def getAccounts():
-    users_list = db.execute_select_all_query(querys.sql_select_all_users)
-    users_list = [Account("Or",1,True,"group_1",100)]
-    return users_list
-
-@account.get('/accounts/{machine_id}',response_class= JSONResponse , status_code= status.HTTP_200_OK)
-def getAccountsByMachine(machine_id):
-    users_list = db.execute_select_all_query(querys.sql_select_all_users_by_machine)
-    # users_list = [User("Or",True,"01/12/2022",1,machine_id,"best group")]
-    return users_list
-
-@account.get('/accounts/stats',response_class= JSONResponse , status_code= status.HTTP_200_OK)
-def getAccountsStats():
-    privilliged_stats = db.execute_select_all_query(querys.sql_select_all_privilliged_machine)
-    stats = {
-                "privilliged_stats": privilliged_stats
-            }
-    return stats
+    accounts_list = get_accounts()
+    return accounts_list
--- repositories/accounts_repository.py
+++ repositories/accounts_repository.py
@@ -1,4 +1,13 @@
 sql_select_all_accounts = """
                            SELECT *
-                           FROM accounts
+                           FROM accounts;
                         """
+sql_select_all_recent_and_done_accounts = """
+                                SELECT a.account_name , a.scan_id , a.is_privileged , a.group_name , a.password_age
+                                FROM accounts a JOIN scan_requests sr ON a.scan_id = sr.scan_id
+                                WHERE sr.is_most_recent = 1 AND sr.scan_status = '3';
+                               """
+
+
+def get_accounts():
+    pass
