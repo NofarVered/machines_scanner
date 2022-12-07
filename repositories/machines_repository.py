bfc850533a491ce34bdf660927c4dc1b543cd76b
MatanNahum <matan.nahum95@gmail.com>
2022-12-07 15:44:21 +0200

    machines by account with not removed

--- repositories/machines_repository.py
+++ repositories/machines_repository.py
@@ -3,7 +3,7 @@ from repositories.sql_wrapper import db_wrapper
 sql_select_machines_by_account = """
                                 SELECT m.machine_id, m.operating_platform, ip_address, ma.enum_status
                                 FROM machines m JOIN machines_accounts ma ON m.machine_id = ma.machine_id
-                                WHERE ma.account_name =  %s
+                                WHERE ma.account_name =  %s AND ma.enum_status != 2 
                                 """
 
 
