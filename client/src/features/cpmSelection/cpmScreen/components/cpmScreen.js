598a94ef76ab190c6b70f70ca921b25a7ac0c898
amit ben basat <amitbasat124@gmail.com>
2022-12-07 13:54:53 +0200

    adding the images in the cpm to each one

--- client/src/features/cpmSelection/cpmScreen/components/cpmScreen.js
+++ client/src/features/cpmSelection/cpmScreen/components/cpmScreen.js
@@ -1,15 +1,20 @@
 import Box from "@mui/material/Box";
 
 import ProfileCard from "./ProfileCard";
+import Elad from "../images/Elad.jpeg";
+import Amit from "../images/Amit.jpeg";
+import Matan from "../images/matan.jpeg";
+//import Nofar from "../images/Nofar.jpeg";
+//import Or from "../images/Or.jpeg";
 
 export default function CpmScreen(props) {
   const cpms = props.cpms;
-  const images = props.images;
+  const images = [Elad,Amit,Matan];
 
   return (
     <Box sx={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}>
-      {cpms.map((cpm) => (
-        <ProfileCard images={images} cpm={cpm} />
+      {cpms.map((cpm,i) => (
+        <ProfileCard images={images[i]} cpm={cpm} />
       ))}
     </Box>
   );
