0e15bccd7820eddd67696f5799236a5d23cddead
MatanNahum <matan.nahum95@gmail.com>
2022-12-07 17:28:33 +0200

    machines for removed account

--- manager/machines_manager.py
+++ manager/machines_manager.py
@@ -9,6 +9,19 @@ db = db_wrapper()
 machine = APIRouter()
 
 
-@machine.get("/machines/{account_name}", response_class=JSONResponse, status_code=status.HTTP_200_OK)
+@machine.get(
+    "/machines/{account_name}",
+    response_class=JSONResponse,
+    status_code=status.HTTP_200_OK,
+)
 def getMachinesByAccount(account_name):
     return Machines_repo.getAllMachinesByAccount(account_name)
+
+
+@machine.get(
+    "/machines/removed/{account_name}",
+    response_class=JSONResponse,
+    status_code=status.HTTP_200_OK,
+)
+def getMachinesByAccount(account_name):
+    return Machines_repo.getAllMachinesByRemovedAccount(account_name)
--- repositories/machines_repository.py
+++ repositories/machines_repository.py
@@ -6,6 +6,12 @@ sql_select_machines_by_account_not_removed = """
                                 WHERE ma.account_name =  %s AND ma.enum_status != 2 
                                 """
 
+sql_select_machines_by_account_removed = """
+                                SELECT m.machine_id, m.operating_platform, ip_address, ma.enum_status
+                                FROM machines m JOIN machines_accounts ma ON m.machine_id = ma.machine_id
+                                WHERE ma.account_name =  %s AND ma.enum_status = 2 
+                                """
+
 
 db = db_wrapper()
 
@@ -14,5 +20,11 @@ class Machines_repo:
     def getAllMachinesByAccount(acount_name):
         machines = db.execute_select_all_query(
             sql_select_machines_by_account_not_removed, (acount_name)
+        )
+        return machines
+
+    def getAllMachinesByRemovedAccount(acount_name):
+        machines = db.execute_select_all_query(
+            sql_select_machines_by_account_removed, (acount_name)
         )
         return machines
