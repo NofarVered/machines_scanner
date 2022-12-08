fb5d44d49563aa313739539795d69f7a6fc83395
amit ben basat <amitbasat124@gmail.com>
2022-12-08 13:49:17 +0200

    adding the part of the add scan

--- client/src/features/scanScreen/ApiScans.js
+++ client/src/features/scanScreen/ApiScans.js
@@ -46,21 +46,21 @@ export function addScan(Scan){
 }
 
 
-export function reRunScan(scanId){
-    const newScan = { 
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(scanId)
-    };
-    return fetch(scansUrl, newScan)
-        .then(response => response.json())
-        .then(
-            (result) => {   
-               return result
-            },           
-            (error) => {              
-              alert(error)
-            }
-          )
-}
+// export function reRunScan(scanId){
+//     const newScan = { 
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(scanId)
+//     };
+//     return fetch(scansUrl, newScan)
+//         .then(response => response.json())
+//         .then(
+//             (result) => {   
+//                return result
+//             },           
+//             (error) => {              
+//               alert(error)
+//             }
+//           )
+// }
 
