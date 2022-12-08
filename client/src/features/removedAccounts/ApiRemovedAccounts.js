1ffd25139fe736472293c4811734517d8cc247fe
eladshachar <elad.shachar@gmail.com>
2022-12-08 12:18:21 +0200

    added removed accounts

---
+++ client/src/features/removedAccounts/ApiRemovedAccounts.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const accountsUrl ='http://localhost:8000/accounts/removed'
+
+
+export function getRemovedAccounts(){
+    return fetch(accountsUrl) 
+    .then(res => res.json())
+    .then(
+      (result) => {  
+                          
+        return result.result
+      },           
+      (error) => {              
+        return error
+      }
+    )
+}
+
+export function getMachinesByRemovedAccount(account) {
+  return fetch(`http://localhost:8000/machines/removed/${account}`) 
+  .then(res => res.json())
+  .then(
+    (result) => {                    
+      return result
+    },           
+    (error) => {              
+      return error
+    }
+  )
+}
+
+export function changeStatusToRemoved(account) {
+  axios.put(``)
+}
