41f354f019327e2eac84cf8baef47343c6532e46
Or Geisler <orgeisler1@gmail.com>
2022-12-08 17:27:14 +0200

    rerun

--- repositories/test.py
+++ repositories/test.py
@@ -1,7 +1,10 @@
-from datetime import datetime
-
-# datetime object containing current date and time
-now = datetime.now()
- # dd/mm/YY H:M:S
-dt_string = now.strftime("%Y-%m-%dT%H:%M:%S")
-print("date and time =", dt_string)	
+ips = [
+    {"ip_addresses ": "192.158.1.38\r"},
+    {"ip_addresses ": "194.167.1.28\r"}
+    ]
+def convert_to_ip_string(ips):
+    ips_string = ""
+    for ip in ips:
+        ips_string +=  ip['ip_addresses '][:-2] + " "
+    return ips_string
+print(convert_to_ip_string(ips))
